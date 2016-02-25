d945394c63f11f677df01348a43cd10c72da4112
manojo <manojo10386@gmail.com>
2016-02-25 17:47:37 +0100

    update scala version

--- project/Build.scala
+++ project/Build.scala
@@ -4,47 +4,46 @@ import Keys._
 object FastParsersBuild extends Build {
 	import PublishSettings._
 
-   def commonSettings = Seq(
-    	version := "0.1-SNAPSHOT",
-	   	scalaVersion := "2.11.5" ,
-       	//scalacOptions := Seq("-optimize"),
-	   	libraryDependencies ++=  Seq(
-		"org.scala-lang" % "scala-compiler"  % scalaVersion.value % "provided",
-		"org.scala-lang" % "scala-reflect" % scalaVersion.value 
-	)
-   ) ++ publishSettings
+  def commonSettings = Seq(
+    version := "0.1-SNAPSHOT",
+	  scalaVersion := "2.11.7" ,
+    //scalacOptions := Seq("-optimize"),
+	  libraryDependencies ++=  Seq(
+		  "org.scala-lang" % "scala-compiler"  % scalaVersion.value % "provided",
+		  "org.scala-lang" % "scala-reflect" % scalaVersion.value
+	  )
+  ) ++ publishSettings
 
 
-   lazy val Examples = Project(
-	id = "Examples",
-	base = file("Examples"),	
-	dependencies = Seq(FastParsers),
-	settings = commonSettings ++ Seq (
-	   // include the macro classes and resources in the main jar
-	   mappings in (Compile, packageBin) ++= mappings.in(FastParsers, Compile, packageBin).value,
-	   // include the macro sources in the main source jar
-	   mappings in (Compile, packageSrc) ++= mappings.in(FastParsers, Compile, packageSrc).value
-	)
-   )
-	
-   lazy val FastParsers = Project(
-	id = "FastParsers",
-	base = file("FastParsers"),	
-	settings = commonSettings ++ publishableSettings ++ Seq (
-		resolvers ++= Seq(
-		  "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"
-		),
-		//addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "0.98.0"),
-			
-		libraryDependencies += "org.scalatest"  % "scalatest_2.11"   % "2.2.1" % "test",
-		
-		libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.5" % "test",
-		
-		libraryDependencies += "com.storm-enroute" % "scalameter_2.11" % "0.6" % "test",
+  lazy val Examples = Project(
+	  id = "Examples",
+	  base = file("Examples"),
+	  dependencies = Seq(FastParsers),
+	  settings = commonSettings ++ Seq (
+	    // include the macro classes and resources in the main jar
+	    mappings in (Compile, packageBin) ++= mappings.in(FastParsers, Compile, packageBin).value,
+	    // include the macro sources in the main source jar
+	    mappings in (Compile, packageSrc) ++= mappings.in(FastParsers, Compile, packageSrc).value
+	  )
+  )
 
-		testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
-  
-    	logBuffered := false
-	) 
-   )
+  lazy val FastParsers = Project(
+    id = "FastParsers",
+    base = file("FastParsers"),
+    settings = commonSettings ++ publishableSettings ++ Seq (
+  	  resolvers ++= Seq(
+  		  "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"
+  		),
+  		//addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "0.98.0"),
+
+  		libraryDependencies ++= Seq (
+        "org.scalatest" %% "scalatest" % "3.0.0-M15" % "test",
+        "org.scalacheck" %% "scalacheck" % "1.11.5" % "test",
+        "com.storm-enroute" % "scalameter_2.11" % "0.6" % "test"
+      ),
+
+  		testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+      logBuffered := false
+  	)
+  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
