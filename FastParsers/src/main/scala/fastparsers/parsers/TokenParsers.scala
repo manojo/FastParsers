cb59eb98cc6b13a71f5dac4970382b9f6c1ab232
manojo <manojo10386@gmail.com>
2016-03-02 00:16:13 +0100

    indentation

--- FastParsers/src/main/scala/fastparsers/parsers/TokenParsers.scala
+++ FastParsers/src/main/scala/fastparsers/parsers/TokenParsers.scala
@@ -11,22 +11,22 @@ import scala.annotation.compileTimeOnly
  */
 trait TokenParsers[StringRepr] {
 
-  @compileTimeOnly("can’t be used outside FastParser")
+  @compileTimeOnly("can't be used outside FastParser")
   implicit def lit(str: StringRepr): Parser[StringRepr] = ???
 
-  @compileTimeOnly("can’t be used outside FastParser")
+  @compileTimeOnly("can't be used outside FastParser")
   def ident: Parser[InputWindow[StringRepr]] = ???
 
-  @compileTimeOnly("can’t be used outside FastParser")
+  @compileTimeOnly("can't be used outside FastParser")
   def number: Parser[InputWindow[StringRepr]] = ???
 
-  @compileTimeOnly("can’t be used outside FastParser")
+  @compileTimeOnly("can't be used outside FastParser")
   def decimalNumber: Parser[InputWindow[StringRepr]] = ???
 
-  @compileTimeOnly("can’t be used outside FastParser")
+  @compileTimeOnly("can't be used outside FastParser")
   def stringLit: Parser[InputWindow[StringRepr]] = ???
 
-  @compileTimeOnly("can’t be used outside FastParser")
+  @compileTimeOnly("can't be used outside FastParser")
   def whitespaces: Parser[InputWindow[StringRepr]] = ???
 
 }
