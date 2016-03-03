460b545c20e8c6d40d4f00ead70c1c4fd5d34c87
manojo <manojo10386@gmail.com>
2016-03-03 19:40:48 +0100

    parsing a small version of a full file

--- FastParsers/src/test/scala/WikiParsersSuite.scala
+++ FastParsers/src/test/scala/WikiParsersSuite.scala
@@ -24,7 +24,7 @@ class WikiParsersSuite extends FunSuite {
     timestamp: TimeStamp,
     contributor: Contributor,
     minor: Boolean,
-    comment: String,
+    comment: Option[String],
     model: String,
     format: String,
     text: String,
@@ -35,8 +35,19 @@ class WikiParsersSuite extends FunSuite {
     title: String,
     ns: Int,
     id: Int,
-    redirect: String,
+    redirect: Option[String],
     rev: Revision
+  )
+
+  case class Namespace(key: String, cs: String, content: String)
+
+  case class SiteInfo(
+    siteName: String,
+    dbName: String,
+    base: String,
+    generator: String,
+    cs: String,
+    namespaces: List[Namespace]
   )
 
   val parser = FastParser {
@@ -58,9 +69,6 @@ class WikiParsersSuite extends FunSuite {
       wsWrap("<redirect") ~> wsWrap("title=") ~> stringLit <~ wsWrap("/>")
     ) map (_.toString)
 
-    /**
-     * start components for revision parser
-     */
     def parentid: Parser[Int] =
       wsWrap("<parentid>") ~> numAsInt <~ wsWrap("</parentid>")
 
@@ -125,7 +133,7 @@ class WikiParsersSuite extends FunSuite {
 
     def revision: Parser[Revision] = (wsWrap("<revision>") ~>
       id ~ parentid ~ timestamp ~
-      contributor ~ minor ~ comment ~ model ~ format ~
+      contributor ~ minor ~ opt(comment) ~ model ~ format ~
       text ~ sha1
     <~ wsWrap("</revision>")) map {
       case (((((((((id, pid), ts), cntr), mnr), cmt), mdl), fmt), txt), sha) =>
@@ -136,10 +144,60 @@ class WikiParsersSuite extends FunSuite {
      * And finally, the page
      */
     def page = (wsWrap("<page>") ~>
-      title ~ ns ~ id ~ redirect ~ revision
+      title ~ ns ~ id ~ opt(redirect) ~ revision
     <~ wsWrap("</page>")) map {
       case ((((t, ns), id), red), rev) => Page(t, ns, id, red, rev)
     }
+
+    def sitename: Parser[String] =
+      wsWrap("<sitename>") ~> takeWhile(_ != '<') <~ wsWrap("</sitename>")
+
+    def dbname: Parser[String] =
+      wsWrap("<dbname>") ~> takeWhile(_ != '<') <~ wsWrap("</dbname>")
+
+    def base: Parser[String] =
+      wsWrap("<base>") ~> takeWhile(_ != '<') <~ wsWrap("</base>")
+
+    def generator: Parser[String] =
+      wsWrap("<generator>") ~> takeWhile(_ != '<') <~ wsWrap("</generator>")
+
+    def cs: Parser[String] =
+      wsWrap("<case>") ~> takeWhile(_ != '<') <~ wsWrap("</case>")
+
+    def namespace1: Parser[Namespace] = (wsWrap("<namespace") ~>
+      (wsWrap("key=") ~> stringLit) ~ (wsWrap("case=") ~> stringLit)
+    ~ (wsWrap('>') ~> takeWhile(_ != '<') <~ wsWrap("</namespace>"))) map {
+      case ((key, cs), ns) => Namespace(key.toString, cs.toString, ns)
+    }
+
+    def namespace2: Parser[Namespace] = (wsWrap("<namespace") ~>
+      (wsWrap("key=") ~> stringLit) ~ (wsWrap("case=") ~> stringLit)
+    <~ wsWrap("/>")) map {
+      case (key, cs) => Namespace(key.toString, cs.toString, "")
+    }
+
+    def namespace = namespace1 | namespace2
+
+    def namespaces: Parser[List[Namespace]] =
+      wsWrap("<namespaces>") ~> rep(namespace) <~ wsWrap("</namespaces>")
+
+    def siteinfo: Parser[SiteInfo] = (wsWrap("<siteinfo>") ~>
+      sitename ~ dbname ~ base ~ generator ~ cs ~ namespaces
+    <~ wsWrap("</siteinfo>")) map { case (((((stn, dbn) ,bs), gen), cs), nss) =>
+        SiteInfo(stn, dbn, bs, gen, cs, nss)
+    }
+
+    def mediawikiOpen: Parser[Unit] = (wsWrap("<mediawiki") ~>
+      wsWrap("xmlns=") ~> wsWrap(stringLit) ~>
+      wsWrap("xmlns:xsi=") ~> wsWrap(stringLit) ~>
+      wsWrap("xsi:schemaLocation=") ~> wsWrap(stringLit) ~>
+      wsWrap("version=") ~> wsWrap(stringLit) ~>
+      wsWrap("xml:lang=") ~> wsWrap(stringLit)
+    ~> wsWrap('>')) map { _ => () }
+
+    def fullParser: Parser[(SiteInfo, List[Page])] = (mediawikiOpen ~>
+      siteinfo ~ rep(page)
+    <~ wsWrap("</mediawiki>"))
 
   }
 
@@ -318,7 +376,7 @@ class WikiParsersSuite extends FunSuite {
         TimeStamp(2014, 10, 26, 4, 50, 23),
         Contributor("Paine Ellsworth", 9092818),
         false,
-        "add [[WP:RCAT|rcat]]s",
+        Some("add [[WP:RCAT|rcat]]s"),
         "wikitext",
         "text/x-wiki",
         """#REDIRECT [[Computer accessibility]]
@@ -333,7 +391,7 @@ class WikiParsersSuite extends FunSuite {
     }
   }
 
-  test("page test1") {
+  test("page test") {
     shouldSucceed(parser.page) {
       """
       <page>
@@ -362,15 +420,15 @@ class WikiParsersSuite extends FunSuite {
         "AfghanistanGeography",
         0,
         14,
-        "Geography of Afghanistan",
+        Some("Geography of Afghanistan"),
         Revision(
           407008307,
           74466619,
           TimeStamp(2011, 1, 10, 3, 56, 19),
           Contributor("Graham87", 194203),
           true,
-          """1 revision from [[:nost:AfghanistanGeography]]: import old edit,
-          see [[User:Graham87/Import]]""",
+          Some("""1 revision from [[:nost:AfghanistanGeography]]: import old edit,
+          see [[User:Graham87/Import]]"""),
           "wikitext",
           "text/x-wiki",
           "#REDIRECT [[Geography of Afghanistan]] {{R from CamelCase}}",
@@ -382,5 +440,119 @@ class WikiParsersSuite extends FunSuite {
     shouldFail(parser.page) {
       "bbacb"
     }
+  }
+
+  test("siteinfo test") {
+    shouldSucceed(parser.siteinfo) {
+      """
+      <siteinfo>
+        <sitename>Wikipedia</sitename>
+        <dbname>enwiki</dbname>
+        <base>https://en.wikipedia.org/wiki/Main_Page</base>
+        <generator>MediaWiki 1.27.0-wmf.10</generator>
+        <case>first-letter</case>
+        <namespaces>
+          <namespace key="-2" case="first-letter">Media</namespace>
+          <namespace key="-1" case="first-letter">Special</namespace>
+          <namespace key="0" case="first-letter" />
+          <namespace key="1" case="first-letter">Talk</namespace>
+          <namespace key="2" case="first-letter">User</namespace>
+          <namespace key="3" case="first-letter">User talk</namespace>
+          <namespace key="4" case="first-letter">Wikipedia</namespace>
+          <namespace key="5" case="first-letter">Wikipedia talk</namespace>
+          <namespace key="6" case="first-letter">File</namespace>
+          <namespace key="7" case="first-letter">File talk</namespace>
+          <namespace key="8" case="first-letter">MediaWiki</namespace>
+          <namespace key="9" case="first-letter">MediaWiki talk</namespace>
+          <namespace key="10" case="first-letter">Template</namespace>
+          <namespace key="11" case="first-letter">Template talk</namespace>
+          <namespace key="12" case="first-letter">Help</namespace>
+          <namespace key="13" case="first-letter">Help talk</namespace>
+          <namespace key="14" case="first-letter">Category</namespace>
+          <namespace key="15" case="first-letter">Category talk</namespace>
+          <namespace key="100" case="first-letter">Portal</namespace>
+          <namespace key="101" case="first-letter">Portal talk</namespace>
+          <namespace key="108" case="first-letter">Book</namespace>
+          <namespace key="109" case="first-letter">Book talk</namespace>
+          <namespace key="118" case="first-letter">Draft</namespace>
+          <namespace key="119" case="first-letter">Draft talk</namespace>
+          <namespace key="446" case="first-letter">Education Program</namespace>
+          <namespace key="447" case="first-letter">Education Program talk</namespace>
+          <namespace key="710" case="first-letter">TimedText</namespace>
+          <namespace key="711" case="first-letter">TimedText talk</namespace>
+          <namespace key="828" case="first-letter">Module</namespace>
+          <namespace key="829" case="first-letter">Module talk</namespace>
+          <namespace key="2300" case="first-letter">Gadget</namespace>
+          <namespace key="2301" case="first-letter">Gadget talk</namespace>
+          <namespace key="2302" case="case-sensitive">Gadget definition</namespace>
+          <namespace key="2303" case="case-sensitive">Gadget definition talk</namespace>
+          <namespace key="2600" case="first-letter">Topic</namespace>
+        </namespaces>
+      </siteinfo>
+      """ gives SiteInfo(
+        "Wikipedia",
+        "enwiki",
+        "https://en.wikipedia.org/wiki/Main_Page",
+        "MediaWiki 1.27.0-wmf.10",
+        "first-letter",
+        List(
+          Namespace("-2", "first-letter", "Media"),
+          Namespace("-1", "first-letter", "Special"),
+          Namespace("0","first-letter", ""),
+          Namespace("1","first-letter", "Talk"),
+          Namespace("2","first-letter", "User"),
+          Namespace("3","first-letter", "User talk"),
+          Namespace("4","first-letter", "Wikipedia"),
+          Namespace("5","first-letter", "Wikipedia talk"),
+          Namespace("6","first-letter", "File"),
+          Namespace("7","first-letter", "File talk"),
+          Namespace("8","first-letter", "MediaWiki"),
+          Namespace("9","first-letter", "MediaWiki talk"),
+          Namespace("10","first-letter", "Template"),
+          Namespace("11","first-letter", "Template talk"),
+          Namespace("12","first-letter", "Help"),
+          Namespace("13","first-letter", "Help talk"),
+          Namespace("14","first-letter", "Category"),
+          Namespace("15","first-letter", "Category talk"),
+          Namespace("100","first-letter", "Portal"),
+          Namespace("101","first-letter", "Portal talk"),
+          Namespace("108","first-letter", "Book"),
+          Namespace("109","first-letter", "Book talk"),
+          Namespace("118","first-letter", "Draft"),
+          Namespace("119","first-letter", "Draft talk"),
+          Namespace("446","first-letter", "Education Program"),
+          Namespace("447","first-letter", "Education Program talk"),
+          Namespace("710","first-letter", "TimedText"),
+          Namespace("711","first-letter", "TimedText talk"),
+          Namespace("828","first-letter", "Module"),
+          Namespace("829","first-letter", "Module talk"),
+          Namespace("2300","first-letter", "Gadget"),
+          Namespace("2301","first-letter", "Gadget talk"),
+          Namespace("2302","case-sensitive", "Gadget definition"),
+          Namespace("2303","case-sensitive", "Gadget definition talk"),
+          Namespace("2600","first-letter", "Topic"))
+        )
+    }
+
+    shouldFail(parser.siteinfo) {
+      "bbacb"
+    }
+  }
+
+  test("smaller xml file test") {
+    import fastparsers.framework.parseresult.{ParseResult, Success, Failure}
+
+    val file = scala.io.Source.fromFile(
+      "FastParsers/src/test/resources/wiki/smaller.xml").getLines mkString "\n"
+
+    val fileArray = file.toCharArray
+    val charSeq = new FastCharSequence(fileArray)
+
+    parser.fullParser(file, 0) match {
+      case Success(result) => println("success kid")
+      case f @ Failure(msg) => fail("error : " + f)
+    }
+
+
   }
 }
