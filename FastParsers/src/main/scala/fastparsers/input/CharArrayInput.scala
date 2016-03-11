9500981fc15fdd92bc70e5ff1a23531453a3c04f
manojo <manojo10386@gmail.com>
2016-03-11 22:37:01 +0100

    huge allocation overhead removed, top catch @jvican

--- FastParsers/src/main/scala/fastparsers/input/CharArrayInput.scala
+++ FastParsers/src/main/scala/fastparsers/input/CharArrayInput.scala
@@ -3,12 +3,15 @@ package fastparsers.input
 /**
  * Created by Eric on 22.04.14.
  */
-trait CharArrayInput extends StringLikeInput {
+trait CharArrayInput extends ArrayLikeInput {
   import c.universe._
 
   def inputType = typeOf[Array[Char]]
+  def inputElemType = typeOf[Char]
+
   override def inputWindowType: c.Type = typeOf[fastparsers.input.InputWindow.CharArrayStruct]
 
   override def getInputWindow(start: c.Tree, end: c.Tree): c.Tree =
     q"new fastparsers.input.InputWindow.CharArrayStruct($inputValue, $start, $end)"
+
 }
